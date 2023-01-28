eecfe37c34b1210d7a9a0a390f4229ce0245e2bc
Sven Helmberger <fforw@gmx.de>
2023-01-28 23:47:32 +0100

    arc pointer test
    test noise pointers based on connected arcs. A really smooth movement at
    a defined speed across noise space

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Simple Starter
+# gen-71
 
-Simple JS build starter for simple, client-only demos.
+WIP
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import domready from "domready"
 import "./style.css"
+import SimplexNoise from "simplex-noise"
+import Color from "./Color"
 
 const PHI = (1 + Math.sqrt(5)) / 2;
 const TAU = Math.PI * 2;
@@ -15,7 +17,96 @@ const config = {
  */
 let ctx;
 let canvas;
+/**
+ * @type CanvasRenderingContext2D
+ */
+let statsCtx;
+let statsCanvas;
+let noise;
 
+class ArcPtr
+{
+    x
+    y
+    x0
+    y0
+    r
+    angle
+    id
+    spin
+    speed
+    lifeTime
+    pow = 1
+    noiseScale = 0
+
+    constructor(x,y,speed, angle = Math.random() * TAU, noiseScale = 0.05)
+    {
+        this.id = Math.random()
+        this.speed = speed
+        this.noiseScale = noiseScale
+        this.init(x,y,angle)
+    }
+
+    randomRadius()
+    {
+        return Math.round(25 + Math.pow(Math.random(),this.pow) * 50)
+    }
+
+    
+    init(x,y,angle, r = this.randomRadius())
+    {
+        const { speed } = this
+        this.x = x
+        this.y = y
+        this.r = r
+        this.angle = angle
+        this.pow = 1 + Math.random() * 2
+        this.spin = speed/(TAU * this.r)
+        this.lifeTime = Math.round(50 + Math.random() * 50)
+    }
+
+    next()
+    {
+        const { x, y, r, angle, spin, id, noiseScale } = this
+
+        const x0 = x + Math.cos(angle) * r
+        const y0 = y + Math.sin(angle) * r
+        if (--this.lifeTime > 0)
+        {
+            this.angle += spin
+
+            this.x0 = x0
+            this.y0 = y0
+        }
+        else
+        {
+            const newRadius = this.randomRadius();
+
+            const combinedRadius = this.r + newRadius
+            const x0 = this.x + Math.cos(angle) * combinedRadius
+            const y0 = this.y + Math.sin(angle) * combinedRadius
+
+            this.speed = -this.speed
+            this.init(
+                x0,y0,
+                angle + TAU * 0.5,
+                newRadius
+            )
+            return this.next()
+        }
+
+        return noise.noise3D(x0 * noiseScale, y0 * noiseScale, id)
+    }
+
+}
+
+function fract(n)
+{
+    return n - Math.floor(n)
+}
+
+const SPEED = 10
+let activeRun = 0
 domready(
     () => {
 
@@ -31,11 +122,73 @@ domready(
         canvas.width = width;
         canvas.height = height;
 
+        statsCanvas = document.createElement("canvas")
+        statsCanvas.width = width
+        statsCanvas.height = Math.round(height * 0.25)
+
+        statsCanvas.style.position = "absolute"
+        statsCanvas.style.bottom = "0"
+        statsCanvas.style.left = "0"
+
+        document.body.appendChild(statsCanvas)
+
+        statsCtx = statsCanvas.getContext("2d")
+
+
         const paint = () => {
+
+            const curr = ++activeRun
 
             ctx.fillStyle = "#000";
             ctx.fillRect(0,0, width, height);
+            statsCtx.clearRect(0,0,width,statsCanvas.height);
 
+            noise = new SimplexNoise()
+
+            const pointers = Array.from({length: 3}).map( () => new ArcPtr(
+                width>>1,
+                height>>1,
+                SPEED,
+            ))
+            const dy = Math.round(height * 0.75)
+            const size = Math.min(width, dy)
+            const dh = statsCanvas.height
+
+            const cx = width >> 1
+            const cy = dy >> 1
+
+            const colors = pointers.map((ptr, idx) => Color.fromHSL(fract(PHI * idx), 0.5, 0.5).toRGBHex())
+
+            const animate = () => {
+
+                for (let i = 0; i < pointers.length; i++)
+                {
+                    let ptr = pointers[i]
+
+                    const v = ptr.next()
+                    ctx.fillStyle = colors[i]
+                    ctx.fillRect( ptr.x0, ptr.y0,2,2)
+
+                    statsCtx.globalCompositeOperation = "copy"
+                    statsCtx.drawImage(
+                        statsCanvas,
+                        2,0,width - 2, dh,
+                        0,0,width - 2, dh
+                    )
+
+                    statsCtx.globalCompositeOperation = "source-over"
+                    statsCtx.clearRect( width - 2, 0,2,dh)
+                    statsCtx.fillStyle = colors[i]
+                    statsCtx.fillRect( width - 2, (0.5 + v * 0.5) * dh,2,2)
+                }
+
+
+                if (curr === activeRun)
+                {
+                    requestAnimationFrame(animate)
+                }
+            }
+            requestAnimationFrame(animate)
         }
 
         paint()
